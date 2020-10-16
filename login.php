e3ce76256664ba516d5ee6c1175929764c98c428
radostin <es.skull@mail.bg>
2020-10-16 08:05:36 +0300

    test

--- login.php
+++ login.php
@@ -1,5 +1,22 @@
 <?php
 
+$connection 
+$servername = "localhost";
+$db = "example";
+$db_username = "root";
+$db_password = "";
+
+try {
+    $this->connection = new PDO(
+        "mysql:host=$servername;dbname=$db",
+        $db_username,
+        $db_password
+    );
+    $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
 function login(){
     $legitUsername = "rado";
     $legitPassword = "1234";
