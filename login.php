7aedcf37a6e4cc570c7e0db86677a75176e9c6fb
radostin <es.skull@mail.bg>
2020-10-16 08:53:40 +0300

    test

--- login.php
+++ login.php
@@ -1,28 +1,39 @@
 <?php
 
-$connection 
-$servername = "localhost";
-$db = "example";
-$db_username = "root";
-$db_password = "";
 
-try {
-    $this->connection = new PDO(
-        "mysql:host=$servername;dbname=$db",
-        $db_username,
-        $db_password
-    );
-    $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+
+function getUserData() {
+    $servername = "localhost";
+    $db = "example";
+    $db_username = "root";
+    $db_password = "";
+    $connection;
+
+    try {
+        $connection = new PDO(
+            "mysql:host=$servername;dbname=$db",
+            $db_username,
+            $db_password
+        );
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+    $sql = "SELECT * FROM user_credentials WHERE username = '".$_POST['username']."' AND 
+                                             password = '".$_POST['password']."'";
+                                             echo $sql;
+    $result = $connection->query($sql);
+    $userData = $result-> fetchAll();
+    if (empty($userData)) {
+        return [];
+    } else {
+        return $userData[0];
+    }
+    return $userData[0];
 }
 
 function login(){
-    $legitUsername = "rado";
-    $legitPassword = "1234";
-        if ($_POST["username"] == $legitUsername && 
-        $_POST["password"] == $legitPassword 
-    ) {
+        if (!empty(getUserData())) {
         header("Location: welcome.html");
     } else {
         header ("Location: index.html");
